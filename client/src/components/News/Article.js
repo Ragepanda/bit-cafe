e63294af2ddd899b6ac4ff3e97b482d1320b8b43
ragepanda <jwoltz13@gmail.com>
2018-10-26 20:39:50 -0400

    modal-with changing title. Created custom IDs and targets

--- client/src/components/News/Article.js
+++ client/src/components/News/Article.js
@@ -23,6 +23,7 @@ class Article extends React.Component {
 			.then(response => {
 				console.log(response.data);
 				const articleText = <ModalFiller></ModalFiller>;
+				ReactDOM.render(this.props.article.title, document.getElementById("title-"+this.props.article.id));
 				ReactDOM.render(articleText, document.getElementById(this.props.article.id));
 
 			})
@@ -37,15 +38,15 @@ class Article extends React.Component {
 					<h6 className="card-subtitle mb-2 text-muted"> Source: {this.props.article.source}</h6>
 					<img src={this.props.article.imageUrl}></img>
 					<p className="card-text mt-3">{this.props.article.body}</p>
-					<button onClick={this.retriveBody} className="btn btn-dark" role="button" data-toggle="modal" data-target="#exampleModalLong">Read More</button>
+					<button onClick={this.retriveBody} className="btn btn-dark" role="button" data-toggle="modal" data-target={"#article-modal-"+this.props.article.id}>Read More</button>
 
 				</div>
 			
-				<div className="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+				<div className="modal fade" id={"article-modal-"+this.props.article.id} tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
 					<div className="modal-dialog modal-lg" role="document">
 						<div className="modal-content md-8">
 							<div className="modal-header">
-								<h5 className="modal-title" id="exampleModalLongTitle">{this.props.article.title}</h5>
+								<h5 className="modal-title" id={"title-"+this.props.article.id}>{this.props.article.title}</h5>
 								<button type="button" className="close" data-dismiss="modal" aria-label="Close">
 									<span aria-hidden="true">&times;</span>
 								</button>
