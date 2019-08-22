82c3c38a5b9b2187e9af6bd62513f0da58263962
unknown <stanjarozews@aol.com>
2019-08-22 13:29:41 -0400

    chart can go daily, hourly, minutely

--- client/src/pages/Chart/Graph.js
+++ client/src/pages/Chart/Graph.js
@@ -10,20 +10,50 @@ class Graph extends React.Component{
 	constructor(props){
 		super(props);
 
+
+
+    this.redoChartDaily = this.redoChartDaily.bind(this);
+    this.redoChartMinutes = this.redoChartMinutes.bind(this);
+    this.redoChartHourly = this.redoChartHourly.bind(this);
+
 		var points = [];
-		var timeLabels = []
-		console.log(this.props.data.length);
+    var pointsMinute =[];
+    var pointsDaily = [];
+		var timeLabelsHour = [];
+    var timeLabelsDay = [];
+    var timeLabelsMinute = [];
 
 		for(var i =0; i < this.props.data.length; i++){
 			points.push({x: i *5, y: this.props.data[i].high});
-			timeLabels.push(this.props.hourConverter(this.props.data[i].time))
-			console.log(points[i]);
+			timeLabelsHour.push(this.props.hourConverter(this.props.data[i].time));
+			//console.log(points[i]);
 		}
+
+    for(var i =0; i < this.props.minutesData.length; i++){
+      pointsMinute.push({x: i *5, y: this.props.minutesData[i].high});
+      timeLabelsMinute.push(this.props.hourConverter(this.props.minutesData[i].time));
+    }
+
+
+    for(var i =0; i < this.props.dailyData.length; i++){
+      pointsDaily.push({x: i *5, y: this.props.dailyData[i].high});
+      timeLabelsDay.push(this.props.multiDayConverter(this.props.dailyData[i].time));
+      //console.log(points[i]);
+    }
 
 
 		this.state={
+      dailyPoints: pointsDaily,
+      minutePoints: pointsMinute,
+      hourPoints: points,
+
+      dayLabel: timeLabelsDay,
+      minuteLabel: timeLabelsMinute,
+      hourLabel: timeLabelsHour,
+
 			chartData: {
-				labels: timeLabels,
+				labels: timeLabelsHour,
+
 				datasets: [{
 					data: points,
 					backgroundColor: 'rgba(224, 218, 242, 0.8)',//fill under line
@@ -72,10 +102,32 @@ class Graph extends React.Component{
 		}	
 	}
 	
+  redoChartDaily(){
+    console.log();
+    this.state.chartData.datasets[0].data = this.state.dailyPoints;
+    this.state.chartData.labels = this.state.dayLabel;
+    let lineChart = this.reference.chartInstance
+    lineChart.update();
+    console.log(this.state.dailyPoints);
+  }
+  redoChartMinutes(){
+    this.state.chartData.datasets[0].data = this.state.minutePoints;
+    this.state.chartData.labels = this.state.minuteLabel;
+    let lineChart = this.reference.chartInstance
+    lineChart.update();
+    console.log(this.state.minutePoints);
+  }
+  redoChartHourly(){
+    this.state.chartData.datasets[0].data = this.state.hourPoints;
+    this.state.chartData.labels = this.state.hourLabel;
+    let lineChart = this.reference.chartInstance
+    lineChart.update();
+    console.log(this.state.hourPoints);
+  }
 
 
 	componentDidMount(){
-		console.log(this.state.chartData);
+		//console.log(this.state.chartData);
 		Chart.plugins.register({
  			 beforeDraw: function(chartInstance) {
     			var ctx = chartInstance.chart.ctx;
@@ -89,15 +141,16 @@ class Graph extends React.Component{
   	{
   		return(
   			<div>
-          <button type="button" className="btn btn-dark">minutes</button>
+          <button type="button" className="btn btn-dark" onClick={this.redoChartMinutes}>minutes</button>
           
-          <button type="button" className="btn btn-dark">hourly</button>
+          <button type="button" className="btn btn-dark" onClick={this.redoChartHourly}>hourly</button>
           
-          <button type="button" className="btn btn-dark">monthly</button>
+          <button type="button" className="btn btn-dark" onClick={this.redoChartDaily}>daily</button>
   				<Line
   					data={this.state.chartData}
   					width={50}
   					options={this.state.options}
+            ref = {(reference) => this.reference = reference}
 				/>
 
   			</div>
