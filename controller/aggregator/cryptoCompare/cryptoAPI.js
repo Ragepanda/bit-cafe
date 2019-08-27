9d2940d320f29a0883fa59c695687ebf4a3d2683
ragepanda <jwoltz13@gmail.com>
2019-08-27 16:36:34 -0400

    News feed DB set up, new DB config added for localhost

--- controller/aggregator/cryptoCompare/cryptoAPI.js
+++ controller/aggregator/cryptoCompare/cryptoAPI.js
@@ -53,11 +53,14 @@ module.exports = {
 
                     // Sends out what's in the DB 
                     else {
-                        res.send(sequelizeArticles);
+        
+                            res.send(sequelizeArticles);
+                              
                     }
                 }
 
                 else {
+
                     // API request to get ccn, bitcoinist, news.bitcoin, coindesk,cointelegraph, 
                     axios.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN&feeds=bitcoinmagazine,bitcoinist,bitcoin.com,ccn,coindesk,cointelegraph&limit=100")
                         .then(response => {
@@ -89,11 +92,63 @@ module.exports = {
             })
     },
 
-    getNewsArticlesBySymbol: function (req, res){
-        var currentTimestamp = new Date() / 1000;
-        axios.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN&feeds=bitcoinmagazine,bitcoinist,bitcoin.com,ccn,coindesk,cointelegraph&limit=100&categories="+req.query.symbol)
-            .then(response =>{
-                var articles = [];
+    getNewsArticlesBySymbol: function (req, res) {
+        db.articles.findAll({ where: { category: req.query.symbol }, order: [['publishedOn', 'DESC']] })
+            .then(sequelizeArticles => {
+                var currentTimestamp = new Date() / 1000;
+
+                if (sequelizeArticles.length > 0) {
+                    if (currentTimestamp - sequelizeArticles[0].createdAt > 600) {
+                        axios.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN&feeds=bitcoinmagazine,bitcoinist,bitcoin.com,ccn,coindesk,cointelegraph&limit=100&categories=" + req.query.symbol)
+                            .then(response => {
+                                var articles = [];
+                                response.data.Data.forEach(element => {
+                                    var entry = {
+                                        source: element.source,
+                                        cryptoId: element.id,
+                                        title: element.title,
+                                        body: element.body.substr(0, 254),
+                                        articleUrl: element.guid,
+                                        imageUrl: element.imageurl,
+                                        createdAt: currentTimestamp,
+                                        publishedOn: element.published_on,
+                                        category: req.query.symbol
+                                    }
+                                    articles.push(entry);
+                                });
+                                // Clear DB before adding new articles
+                                db.articles.destroy({
+                                    where: {category: req.query.symbol}
+                                })
+                                    .then(() => {
+                                        // Add initial set of articles
+                                        db.articles.bulkCreate(articles)
+                                            .then(() => {
+                                                db.articles.findAll({where:{category: req.query.symbol}})
+                                                    .then(updatedResponse => {
+                                                        res.send(updatedResponse);
+                                                    })
+                                            })
+                                    })
+
+
+                            });
+                    }
+
+                    else{
+                        // sends what's already there
+                        db.articles.findAll({where:{category: req.query.symbol}})
+                        .then(sequelizeArticles => {
+                            res.send(sequelizeArticles);
+                        })       
+                    }
+
+                 }
+
+                else {
+                    axios.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN&feeds=bitcoinmagazine,bitcoinist,bitcoin.com,ccn,coindesk,cointelegraph&limit=100&categories=" + req.query.symbol)
+                        .then(response => {
+                            var articles = [];
                             response.data.Data.forEach(element => {
                                 var entry = {
                                     source: element.source,
@@ -103,19 +158,32 @@ module.exports = {
                                     articleUrl: element.guid,
                                     imageUrl: element.imageurl,
                                     createdAt: currentTimestamp,
-                                    publishedOn: element.published_on
+                                    publishedOn: element.published_on,
+                                    category: req.query.symbol
                                 }
                                 articles.push(entry);
-                                
+
                             });
-                            console.log(response);
-                            res.send(response.data.Data);
-            })
-            .catch(error =>{
-                console.log(error);
-                res.send(error);
+                             // Clear DB before adding new articles                        
+                                    // Add initial set of articles
+                                    db.articles.bulkCreate(articles)
+                                        .then(() => {
+                                            db.articles.findAll({where: {category: req.query.symbol}})
+                                                .then(updatedResponse => {
+                                                    res.send(updatedResponse);
+                                                })
+                                        })
+                                
+                            // console.log(response);
+                            // res.send(response.data.Data);
+                        })
+                        .catch(error => {
+                            console.log(error);
+                            res.send(error);
+                        })
+
+                }
             })
     }
-
 
 }
