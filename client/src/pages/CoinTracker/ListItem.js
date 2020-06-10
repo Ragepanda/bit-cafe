ea54fae4d5e829614a009f93a7732bf1143bd3ae
ragepanda <jwoltz13@gmail.com>
2020-06-10 13:05:06 -0400

    link changes

--- client/src/pages/CoinTracker/ListItem.js
+++ client/src/pages/CoinTracker/ListItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './ListItem.css';
+import {Link } from 'react-router-dom'
 
 class ListItem extends React.Component{
 	state = {
@@ -18,13 +19,13 @@ class ListItem extends React.Component{
 		return(
 				
 				<tr className="rowHover">
-					<th scope="row"><div className="bigDiv"><a className="bigLink" href={"/chart/"+this.props.coins.symbol}>{this.props.rank+1}</a></div></th>
-					<td><div className="bigDiv"><a className="bigLink" href={"/chart/"+this.props.coins.symbol}><img alt="" height="15px" width="15px" src={"https://www.cryptocompare.com"+this.props.coins.imageUrl}/> {this.props.coins.fullName} ({this.props.coins.symbol})</a></div></td>
-					<td><div className="bigDiv"><a className="bigLink" href={"/chart/"+this.props.coins.symbol}>{this.props.coins.marketCapString.replace(/\s/g,'')}</a></div></td>
-					<td><div className="bigDiv"><a className="bigLink" href={"/chart/"+this.props.coins.symbol}>{this.props.coins.price.replace(/\s/g,'')}</a></div></td>
-					<td><div className="bigDiv"><a className="bigLink" href={"/chart/"+this.props.coins.symbol}>{this.props.coins.volume24H.replace(/\s/g,'')}</a></div></td>
-					<td><div className="bigDiv"><a className="bigLink" href={"/chart/"+this.props.coins.symbol}>{this.props.coins.supply}</a></div></td>
-					<td className={this.state.color}><div className="bigDiv"><a className="bigLink" href={"/chart/"+this.props.coins.symbol}>{Math.round(100*parseFloat(this.props.coins.changePct24))/100 + "%"}</a></div></td>
+					<th scope="row"><div className="bigDiv"><Link className="bigLink" to={"/chart/"+this.props.coins.symbol}>{this.props.rank+1}</Link></div></th>
+					<td><div className="bigDiv"><Link className="bigLink" to={"/chart/"+this.props.coins.symbol}><img alt="" height="15px" width="15px" src={"https://www.cryptocompare.com"+this.props.coins.imageUrl}/> {this.props.coins.fullName} ({this.props.coins.symbol})</Link></div></td>
+					<td><div className="bigDiv"><Link className="bigLink" to={"/chart/"+this.props.coins.symbol}>{this.props.coins.marketCapString.replace(/\s/g,'')}</Link></div></td>
+					<td><div className="bigDiv"><Link className="bigLink" to={"/chart/"+this.props.coins.symbol}>{this.props.coins.price.replace(/\s/g,'')}</Link></div></td>
+					<td><div className="bigDiv"><Link className="bigLink" to={"/chart/"+this.props.coins.symbol}>{this.props.coins.volume24H.replace(/\s/g,'')}</Link></div></td>
+					<td><div className="bigDiv"><Link className="bigLink" to={"/chart/"+this.props.coins.symbol}>{this.props.coins.supply}</Link></div></td>
+					<td className={this.state.color}><div className="bigDiv"><Link className="bigLink" to={"/chart/"+this.props.coins.symbol}>{Math.round(100*parseFloat(this.props.coins.changePct24))/100 + "%"}</Link></div></td>
 									
 				</tr>
 	)}
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const db = require("./models");
 const path = require("path");
-const cors = require("cors");
+
 
 app.use(cors({
   origin:["http://localhost:3000","http://localhost:45678", "http://bitcoinsama.com"],
